9ff7f856d4e7d27d4ba5895044021c42f0d0b189
Michele Arcopinto <150271708+michelearcopinto@users.noreply.github.com>
2024-02-07 14:49:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ExampleName from "./components/ExampleName.vue";
+
+export default {
+  components: {
+    ExampleName,
+  },
+};
+</script>
+
+<template>
+  <h1>Hello World</h1>
+
+  <ExampleName />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
